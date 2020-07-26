a57e70acc7c7ad77e9b34c7713e421198ee5e67f
Henry Laur <hlaur1344@gmail.com>
2020-07-26 12:11:08 +0300

    readme.md

--- src/main/java/com/chatroom/chatroom/ChatroomApplication.java
+++ src/main/java/com/chatroom/chatroom/ChatroomApplication.java
@@ -1,17 +1,11 @@
 package com.chatroom.chatroom;
 
-import com.chatroom.chatroom.controller.CurrentURL;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 @SpringBootApplication
 public class ChatroomApplication {
@@ -24,7 +18,7 @@ public class ChatroomApplication {
 		CorsConfiguration config = new CorsConfiguration();
 
 		config.setAllowCredentials(false);
-		config.addAllowedOrigin("http://" + CurrentURL.URL + ":3000");
+		config.addAllowedOrigin("*");
 		config.addAllowedHeader("*");
 		config.addAllowedMethod("GET");
 		config.addAllowedMethod("POST");
--- src/main/java/com/chatroom/chatroom/controller/CurrentURL.java
+++
@@ -1,5 +0,0 @@
-package com.chatroom.chatroom.controller;
-
-public class CurrentURL {
-    public final static String URL = "localhost";
-}
